358622bf30cffcadd4c51f8af402c7bdb18046b8
Valeriya <tmcxezsim5nn@mail.ru>
2021-10-09 12:36:45 +0300

    v2

---
+++ src/main/java/domain/DonutInfo.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class DonutInfo {
+
+    private boolean isDonut;
+    private int paidDuration;
+    private boolean canPublishFreeCopy;
+    private String editMode;
+
+    public boolean getIsDonut() {
+        return isDonut;
+    }
+
+    public void setIsDonut(boolean isDonut) {
+        this.isDonut = isDonut;
+    }
+
+    public int getPaidDuration() {
+        return paidDuration;
+    }
+
+    public void setPaidDuration(int paidDuration) {
+        this.paidDuration = paidDuration;
+    }
+
+    public boolean getCanPublishFreeCopy() {
+        return canPublishFreeCopy;
+    }
+
+    public void setCanPublishFreeCopy(boolean canPublishFreeCopy) {
+        this.canPublishFreeCopy = canPublishFreeCopy;
+    }
+
+    public String getEditMode() {
+        return editMode;
+    }
+
+    public void setEditMode(String editMode) {
+        this.editMode = editMode;
+    }
+}
--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -4,12 +4,32 @@ public class Post {
     private CommentsInfo commentsInfo;
     private LikesInfo likesInfo;
     private AuthorInfo authorInfo;
+    private Copyright copyright;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+    private Geo geo;
+    private PostSource postSource;
+    private DonutInfo donutInfo;
     private int postId;
     private int administratorId;
     private int ownerId;
     private int timePublication;
-    private String imageUrl;
-    private String postText;
+    private int fromId;
+    private int createdBy;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private int signerId;
+    private int postponedId;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean isPinned;
+    private boolean markedAsAds;
+    private boolean isFavorite;
+    private String[] imageUrl;
+    private String[] postText;
+    private String[] postType;
 
     public CommentsInfo getCommentsInfo() {
         return commentsInfo;
@@ -33,6 +53,54 @@ public class Post {
 
     public void setLikesInfo(AuthorInfo authorInfo) {
         this.authorInfo = authorInfo;
+    }
+
+    public Copyright getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(Copyright commentsInfo) {
+        this.copyright = copyright;
+    }
+
+    public RepostsInfo getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(RepostsInfo repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public ViewsInfo getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(ViewsInfo viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public PostSource getPostSource() {
+        return postSource;
+    }
+
+    public void setPostSource(PostSource postSource) {
+        this.postSource = postSource;
+    }
+
+    public DonutInfo getDonutInfo() {
+        return donutInfo;
+    }
+
+    public void setDonutInfo(DonutInfo donutInfo) {
+        this.donutInfo = donutInfo;
     }
 
     public int getPostId() {
@@ -67,19 +135,131 @@ public class Post {
         this.timePublication = timePublication;
     }
 
-    public String getImageUrl() {
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
+    }
+
+    public boolean getCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(boolean canPin) {
+        this.canPin = canPin;
+    }
+
+    public boolean getCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public boolean getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(boolean canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public boolean getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIsPinned(boolean isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public boolean getMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(boolean markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean getIsFavorite() {
+        return isFavorite;
+    }
+
+    public void setIsFavorite(boolean isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+
+    public String[] getImageUrl() {
         return imageUrl;
     }
 
-    public void setImageUrl(String imageUrl) {
+    public void setImageUrl(String[] imageUrl) {
         this.imageUrl = imageUrl;
     }
 
-    public String getPostText() {
+    public String[] getPostText() {
         return postText;
     }
 
-    public void setPostText(String postText) {
+    public void setPostText(String[] postText) {
         this.postText = postText;
+    }
+
+    public String[] getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String[] postType) {
+        this.postType = postType;
     }
 }
